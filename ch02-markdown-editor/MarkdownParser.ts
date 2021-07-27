7e14507f6615bf94e691044d664f289c83253163
paikwiki <paikwiki@gmail.com>
2021-07-28 02:39:56 +0900

    ch02: Study Chapter02(Visitor Pattern, WIP)

--- ch02-markdown-editor/MarkdownParser.ts
+++ ch02-markdown-editor/MarkdownParser.ts
@@ -11,6 +11,73 @@ interface IMarkdownDocument {
   Get(): string;
 }
 
+interface IVisitor {
+  Visit(token: ParseElement, MarkdownDocument: IMarkdownDocument): void;
+}
+
+interface IVisitable {
+  Accept(
+    visitor: IVisitor,
+    toke: ParseElement,
+    markdownDocument: IMarkdownDocument
+  ): void;
+}
+
+abstract class VisitorBase implements IVisitor {
+  constructor(
+    private readonly tagType: TagType,
+    private readonly TagTypeToHtml: TagTypeToHtml
+  ) {}
+
+  public Visit(token: ParseElement, markdownDocument: IMarkdownDocument): void {
+    markdownDocument.Add(
+      this.TagTypeToHtml.OpengingTag(this.tagType),
+      token.CurrentLine,
+      this.TagTypeToHtml.ClosingTag(this.tagType)
+    );
+  }
+}
+
+class Header1Visitor extends VisitorBase {
+  constructor() {
+    super(TagType.Header1, new TagTypeToHtml());
+  }
+}
+
+class Header2Visitor extends VisitorBase {
+  constructor() {
+    super(TagType.Header2, new TagTypeToHtml());
+  }
+}
+
+class Header3Visitor extends VisitorBase {
+  constructor() {
+    super(TagType.Header3, new TagTypeToHtml());
+  }
+}
+
+class ParagraphVisitor extends VisitorBase {
+  constructor() {
+    super(TagType.Paragraph, new TagTypeToHtml());
+  }
+}
+
+class HorizontalRuleVisitor extends VisitorBase {
+  constructor() {
+    super(TagType.HorizontalRule, new TagTypeToHtml());
+  }
+}
+
+class Visitable implements IVisitable {
+  public Accept(
+    visitor: IVisitor,
+    token: ParseElement,
+    markdownDocument: IMarkdownDocument
+  ): void {
+    visitor.Visit(token, markdownDocument);
+  }
+}
+
 class MarkdownDocument implements IMarkdownDocument {
   private content: string = "";
 
