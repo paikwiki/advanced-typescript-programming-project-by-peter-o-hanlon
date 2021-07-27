a50125c45d82c879fa83ca450560a326a2837fe1
paikwiki <paikwiki@gmail.com>
2021-07-27 20:51:07 +0900

    Study Chapter02(WIP)

---
+++ ch02-markdown-editor/MarkdownParser.ts
@@ -0,0 +1,72 @@
+enum TagType {
+  Paragraph,
+  Header1,
+  Header2,
+  Header3,
+  HorizontalRule,
+}
+interface IMarkdownDocument {
+  Add(...content: string[]) : void;
+  Get(): string;
+}
+
+class MarkdownDocument implements IMarkdownDocument {
+  private content: string = "";
+  Add(...content: string[]): void {
+    content.forEach(element => {
+      this.content += element;
+    });
+  }
+  Get(): string {
+    return this.content;
+  }
+}
+
+class ParseElement {
+  CurrentLine: string = "";
+}
+
+class TagTypeToHtml {
+  private readonly tagType: Map<TagType, string> = new Map<TagType, string>();
+
+  constructor() {
+    this.tagType.set(TagType.Header1, "h1");
+    this.tagType.set(TagType.Header2, "h2");
+    this.tagType.set(TagType.Header3, "h3");
+    this.tagType.set(TagType.Paragraph, "p");
+    this.tagType.set(TagType.HorizontalRule, "hr");
+  }
+
+  private GetTag(tagType: TagType, openingTagPattern: string) : string {
+    let tag = this.tagType.get(tagType);
+    if (tag !== null) {
+      return `${openingTagPattern}${tag}>`;
+    }
+    return `${openingTagPattern}p>`;
+  }
+
+  public OpeningTag(tagType: TagType): string {
+    return this.GetTag(tagType, `<`);
+  }
+
+  public ClosingTag(tagType: TagType): string {
+    return this.GetTag(tagType, `</`);
+  }
+
+}
+
+class HtmlHandler {
+  public TextChangeHandler(id: string, output: string): void {
+    let markdown = <HTMLTextAreaElement>document.getElementById(id);
+    let markdownOutput = <HTMLLabelElement>document.getElementById(output);
+    if (markdown !== null) {
+      markdown.onkeyup = (e) => {
+        if (markdown.value) {
+          markdownOutput.innerHTML = markdown.value;
+        } else {
+          markdownOutput.innerHTML = "<p></p>";
+        }
+      };
+    }
+  }
+}
